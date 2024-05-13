09a8ca8c95288d29a246b04ba229c93d7899fcba
Abhinav Kumar <dynamic2106@gmail.com>
2024-05-13 12:00:02 +0530

    Added projects Added EF core Dbcontext Added DI extension services to register Dbcontext

---
+++ Data Access layer/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Business_Logic_Layer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_layer.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+        }
+        public DbSet<ProductDetails> productDetails { get; set; }
+    }
+}
---
+++ Data Access layer/DepedencyIjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Data_Access_layer.Context;
+
+namespace Data_Access_layer
+{
+    public static class DepedencyIjection
+    {
+        public static void RegisterDALDepedencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+        }
+    }
+}
