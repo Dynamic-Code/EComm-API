b5d2a68c68aca0edb8beaaa3cd72e42e29c86493
Abhinav Kumar <dynamic2106@gmail.com>
2024-05-13 11:58:18 +0530

    Added ProductDetails Models

---
+++ Business Logic Layer/Models/ProductDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Logic_Layer.Models
+{
+    public class ProductDetails
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
